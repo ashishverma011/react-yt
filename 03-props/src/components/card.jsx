1f632be226a6d16bf772b33e701cf50919b3e3a4
Delta Student <deltastudent@apnacollege>
2026-08-20 23:08:23 +0530

    Initial commit

---
+++ 03-props/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = (props) => {
+
+    return (
+        <div className="card">
+            <img src={props.img} alt="" />
+            <h1>{props.user}</h1>
+            <p>Lorem ipsum dolor sit amet..</p>
+            <button>View Profile</button>
+        </div>
+    )
+}
+
+export default Card
